c91b28e23fae94fcb9c2918787042fa39d0d0b2b
machenbach <machenbach@vmware.com>
2015-12-16 19:49:14 -0800

    recursion works, too slow. First attempt at Meomization a drastic failure.

--- brickgame/src/Solution.java
+++ brickgame/src/Solution.java
@@ -1,13 +1,56 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
+	class QNode {
+		int p;
+		int qlen;
+		
+		public QNode(int p, LinkedList<Integer> q) {
+			this.p = p;
+			qlen = q.size();
+		}
+
+		@Override
+		public int hashCode() {
+			return Integer.hashCode(p + qlen);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			QNode other = (QNode) obj;
+			if (p != other.p)
+				return false;
+			if (qlen != other.qlen)
+				return false;
+			return true;
+		}
+
+		
+	}
 	
-	long[] score; // player 0, player 1
+	Map<QNode, long[]> states; 
 	
 	LinkedList<Integer> bricks;  // stack of bricks
 	
+	@SuppressWarnings("unchecked")
 	long[] play(int p, LinkedList<Integer> q) {
+		
+		// if we've seen this state, return the values
+		QNode n = new QNode(p, q);
+		if (states.containsKey(n)) {
+			System.out.println("hit");
+			return states.get(n);
+		}
+		
 		// if the queue has three or few elements in it, best move is take them all
 		long[] res = new long[2];
 		if (q.size() <= 3) {
@@ -16,8 +59,11 @@ public class Solution {
 				val += i;
 			}
 			res[p] = val;
+			states.put(n, res.clone());
 			return res;
 		}
+		
+		// Otherwise, we need to search for it
 		long ret = Long.MIN_VALUE;
 		int val = 0;
 		LinkedList<Integer> q1 = q;
@@ -27,16 +73,17 @@ public class Solution {
 			long[] val2 = play(p == 0 ? 1 : 0, q1);
 			if (ret < val + val2[p]) {
 				res = val2;
-				ret = val;
+				ret = val + val2[p];
 			}
 		}
-		res[p] += ret;
+		res[p] = ret;
+		states.put(n,res.clone());
 		return res;
 	}
 	
 	public long play() {
 		long [] ret = play(0, bricks);
-		return Math.max(ret[0], ret[1]);
+		return ret[0];
 	}
 	
 	public void init(Scanner in) {
@@ -45,6 +92,7 @@ public class Solution {
 		for (int i = 0; i < n; i++) {
 			bricks.add(in.nextInt());
 		}
+		states = new HashMap<>();
 	}
 
 	public static void main(String[] args) {
