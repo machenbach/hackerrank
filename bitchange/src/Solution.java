b8bfd8377ecc54244be4c6d3fe5297e0c3cd524b
machenbach <machenbach@vmware.com>
2015-12-18 15:41:43 -0800

    Not working at all, but fast

--- bitchange/src/Solution.java
+++ bitchange/src/Solution.java
@@ -1,24 +1,66 @@
 
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
+	
+	class BigBit {
+		int bitSize;
+		int byteCnt;
+		byte[] bits;
+		
+		public BigBit(int bitSize, BigInteger val) {
+			this.bitSize = bitSize;
+			byteCnt = (bitSize >> 8) + 1;
+			bits = Arrays.copyOf(val.toByteArray(), byteCnt);
+		}
+		
+		public BigBit(BigBit b) {
+			bitSize = b.bitSize;
+			byteCnt = b.byteCnt;
+			bits = b.bits.clone();
+		}
+		
+		public void setBit(int idx, int bit) {
+			int bdx = idx >> 8;
+			int mask = 0x1 << (idx % 8);
+			if (bit == 0) {
+				mask = -1 ^ mask;
+				bits[bdx] = (byte) (bits[bdx] & mask);
+			}
+			else {
+				bits[bdx] = (byte) (bits[bdx] | mask);
+			}
+		}
+		
+		public int testBit(int idx) {
+			int bdx = idx >> 8;
+			int mask = 1 << (idx % 8);
+			return (bits[bdx] & mask) == 0 ? 0 : 1;
+		}
+		
+		public void addTo (BigBit o) {
+			int lastCarry = 0;
+			int nextCarry = 0;
+			for (int i = 0; i < byteCnt; i++) {
+				nextCarry = (bits[i] & o.bits[i]) >> 7;
+				bits[i] += (o.bits[i] + lastCarry);
+				lastCarry = nextCarry;
+			}
+		}
+	}
 
-	BigInteger a;
-	BigInteger b;
-	BigInteger c;
+	BigBit a;
+	BigBit b;
+	BigBit c;
 	
 	boolean cDirty = true;
 	
-	public void init(Scanner in) {
-		a = in.nextBigInteger(2);
-		b = in.nextBigInteger(2);
-	}
-	
-	void changeBit(BigInteger v, int idx, int bit) {
-		if (bit == 0) { v.clearBit(idx); }
-		else { v.setBit(idx); }
+	public void init(int n, Scanner in) {
+		a = new BigBit(n, in.nextBigInteger(2));
+		b = new BigBit(n, in.nextBigInteger(2));
 	}
 	
 	public void parse(Scanner in) {
@@ -29,28 +71,23 @@ public class Solution {
 		switch (cmd) {
 			case "set_a":
 				bit = in.nextInt();
-				if (bit == 0) { 
-					a = a.clearBit(idx); }
-				else { 
-					a = a.setBit(idx); }
+				a.setBit(idx, bit);;
 				cDirty = true;
 				break;
 				
 			case "set_b":
 				bit = in.nextInt();
-				if (bit == 0) { 
-					b = b.clearBit(idx); }
-				else { 
-					b = b.setBit(idx); }
+				b.setBit(idx, bit);
 				cDirty = true;
 				break;
 				
 			case "get_c":
 				if (cDirty) {
-					c = a.add(b);
+					c = new BigBit(a);
+					c.addTo(b);
 					cDirty = false;
 				}
-				System.out.print(c.testBit(idx) ? "1" : "0");
+				System.out.print(c.testBit(idx));
 		}
 		
 	}
@@ -59,7 +96,7 @@ public class Solution {
 		int n = in.nextInt();
 		int q = in.nextInt();
 		Solution s = new Solution();
-		s.init(in);
+		s.init(n, in);
 		for (int i = 0; i < q; i++) {
 			s.parse(in);
 		}
