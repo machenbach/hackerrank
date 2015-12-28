dbfac976f5ee29e2b7b666e5f6236877db410115
machenbach <machenbach@vmware.com>
2015-12-28 11:04:57 -0800

    Working version.  Still don't really understand the reason it works yet

--- xorninja/src/Solution.java
+++ xorninja/src/Solution.java
@@ -1,65 +1,28 @@
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Solution {
+	public static long xorSum(int n, Scanner in) {
+		BigInteger tot = ZERO;
+		for (int i = 0; i < n; i++) {
+			tot = tot.or(in.nextBigInteger());
+		}
+		BigInteger twoNminusOne = ONE.shiftLeft(n-1);
+		tot = tot.multiply(twoNminusOne);
+		return tot.mod(BigInteger.valueOf(1000000007l)).longValue();
+	}
+	
 	public static void main(String[] args) {
-		int[] ary = { 1, 2, 3, 4};
-		int n = 4;
-		
-		int tot = 0;
-		// ones
-		for (int i = 0; i < n; i++) {
-			System.out.println(" " + ary[i]);
-			tot += ary[i];
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = in.nextInt();
+			System.out.println(xorSum(n, in));
 		}
-		
-		tot = 0;
-		// twos
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				int t = ary[i] ^ ary[j];
-				System.out.println(" " + ary[i] + " " + ary[j] + ": " + t);
-				tot += t;
-			}
-		}
-		System.out.println(tot);
-		tot = 0;
-		// twos
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				int t = ary[i] ^ ary[j];
-				System.out.println(" " + ary[i] + " " + ary[j] + ": " + t);
-				tot += t;
-			}
-		}
-		System.out.println(tot);
-
-		// threes
-		tot = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				for (int k = j + 1; k < n; k++) {
-					int t = ary[i] ^ ary[j] ^ ary[k];
-					System.out.println(" " + ary[i] + " " + ary[j] + " " + ary[k] + ": " + t);
-					tot += t;
-				}
-			}
-		}
-		System.out.println(tot);
-		tot = 0;
-		// all of them
-		int t = 0;
-		for (int i = 0; i < n; i++) {
-			t ^= ary[i];
-			System.out.print(" " + ary[i]);
-		}
-		System.out.println(": " + t);
-		tot += t;
-		
-		for (int i = 0; i < n; i++) {
-			System.out.println(" " + (t ^ ary[i]));
-			
-		}
-		
-		System.out.println(tot);
+		in.close();
 	}
 
 }
