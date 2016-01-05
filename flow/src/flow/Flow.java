da5a61112de03f130a7271726f34ce1e2ed52c37
machenbach <machenbach@vmware.com>
2016-01-05 08:20:44 -0800

    First commit.  Playing with flows

---
+++ flow/src/flow/Flow.java
@@ -0,0 +1,200 @@
+package flow;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Flow {
+	class Pair<T> {
+		public T u;
+		public T v;
+		
+		public Pair(T u, T v) {
+			this.u = u;
+			this.v = v;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((u == null) ? 0 : u.hashCode());
+			result = prime * result + ((v == null) ? 0 : v.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			Pair<?> other = (Pair<?>) obj;
+			if (!getOuterType().equals(other.getOuterType())) {
+				return false;
+			}
+			if (u == null) {
+				if (other.u != null) {
+					return false;
+				}
+			} else if (!u.equals(other.u)) {
+				return false;
+			}
+			if (v == null) {
+				if (other.v != null) {
+					return false;
+				}
+			} else if (!v.equals(other.v)) {
+				return false;
+			}
+			return true;
+		}
+
+		private Flow getOuterType() {
+			return Flow.this;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("(%s, %s)", u, v);
+		}
+
+		
+	}
+
+	Map<Pair<Integer>, Integer> f; // flow function
+	Map<Pair<Integer>, Integer> c; // capacity
+	Map<Pair<Integer>, Integer> cn; // induced capacity
+	Map<Integer, List<Integer>> gn; // induced graph
+	List<Integer> V;				// vertices in the graph (Edges are c)
+	
+	int s = 0;
+	int t = Integer.MAX_VALUE;
+	
+	public void init() {
+		Integer[] nodes = {s, 1, 2, 3, 4, t};
+		V = Arrays.asList(nodes);
+		f = new HashMap<>();
+		
+		c = new HashMap<>();
+		c.put(new Pair<>(s, 1), 16);
+		c.put(new Pair<>(s, 2), 13);
+		c.put(new Pair<>(1, 2), 10);
+		c.put(new Pair<>(2, 4), 14);
+		c.put(new Pair<>(1, 3), 12);
+		c.put(new Pair<>(2, 1), 4);
+		c.put(new Pair<>(3, 2), 9);
+		c.put(new Pair<>(2, 4), 14);
+		c.put(new Pair<>(4, 3), 7);
+		c.put(new Pair<>(3, t), 20);
+		c.put(new Pair<>(4, t), 4);
+	}
+	
+	// use the current flows to create a new residual graph
+	void createResidual() {
+		cn = new HashMap<>();
+		gn = new HashMap<>();
+		
+		for (int u : V) {
+			for (int v : V) {
+				Pair<Integer> p = new Pair<Integer>(u, v);
+				int f1 = f.getOrDefault(p, 0);
+				int c1 = c.getOrDefault(p, 0);
+				int r = c1 - f1;
+				if (r > 0) {
+					cn.put(p, r);
+					gn.putIfAbsent(u, new ArrayList<>());
+					gn.get(u).add(v);
+				}
+			}
+		}
+	}
+	
+	// find a new flow through the residual graph
+	int findFlow(List<Integer> path) {
+		int flow = Integer.MAX_VALUE;
+		Queue<Integer> q = new ArrayDeque<>();
+		Set<Integer> seen = new HashSet<>();
+		// initialize with the starting vertex.
+		q.add(s);
+		seen.add(s);
+		path.add(s);
+		while (!q.isEmpty()) {
+			int n = q.poll();
+			if (n == t) {
+				return flow;
+			}
+			for (int c : gn.getOrDefault(n, new ArrayList<Integer>())) {
+				if (!seen.contains(c)) {
+					seen.add(c);
+					q.add(c);
+				}
+				flow = Math.min(flow, cn.getOrDefault(new Pair<>(n, c), 0));
+				path.add(c);
+			}
+		}
+		return 0;
+	}
+	
+	void addAugmenting(int flow, List<Integer> path) {
+		int u = -1;
+		for (int v : path) {
+			if (u != -1) {
+				Pair<Integer> p = new Pair<>(u, v);
+				Pair<Integer> antip = new Pair<>(v, u);
+				f.put(p, f.getOrDefault(p, 0) + flow);
+				f.put(antip, f.getOrDefault(p, 0) - flow);
+			}
+			u = v;
+		}
+		
+	}
+	
+	public void solve() {
+		// loop until there are no more flows
+		while(true) {
+			List<Integer> path = new ArrayList<>();
+			createResidual();
+			int flow = findFlow(path);
+			if (flow == 0) {
+				return;
+			}
+			addAugmenting(flow, path);
+		}
+		
+	}
+	
+	public void printGraph() {
+		Set<Integer> nodes = new TreeSet<>(V);
+		int u = -1;
+		for (int v : nodes) {
+			if (u != -1) {
+				Pair<Integer> p = new Pair<>(u, v);
+				System.out.println(String.format("%s: %s/%s", p, f.getOrDefault(p, 0), c.get(p)));
+			}
+			u = v;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Flow f = new Flow();
+		f.init();
+		f.solve();
+		f.printGraph();
+
+	}
+
+}
