affa775c29b2a5dde776abec154a939dd6fac103
machenbach <machenbach@vmware.com>
2016-01-29 17:39:34 -0800

    Solution2 uses different method complete, and gives the right answer. Need to work for efficency at this point.

---
+++ cuttrees/src/Solution2Test.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution2Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() throws FileNotFoundException {
+		System.setIn(new FileInputStream("test.txt"));
+		Solution2.main(null);
+	}
+
+	@Test
+	public void test2() throws FileNotFoundException {
+		System.out.println("Test 2");
+		System.setIn(new FileInputStream("test2.txt"));
+		Solution2.main(null);
+	}
+
+}
