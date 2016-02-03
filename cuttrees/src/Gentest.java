74ddb18d887500e73d118c3a4b3851e08039ecc3
machenbach <machenbach@vmware.com>
2016-02-02 16:31:27 -0800

    Solution 3, convert graph to tree, recurse on tree.
    Fast but wrong.

--- cuttrees/src/Gentest.java
+++ cuttrees/src/Gentest.java
@@ -15,8 +15,21 @@ public class Gentest {
 		}
 	}
 
+	static void test4() {
+		try (PrintWriter pr = new PrintWriter("test4.txt")) {
+			pr.println("50 25");
+			for (int i = 2; i <= 50; i++) {
+				pr.println(i + " " + (i-1));
+			}
+			pr.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
-		test3();
+		test4();
 	}
 
 }
---
+++ cuttrees/src/Solution3Test.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Solution3Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() throws FileNotFoundException {
+		System.setIn(new FileInputStream("test.txt"));
+		Solution3.main(null);
+	}
+
+	@Test
+	public void test2() throws FileNotFoundException {
+		System.setIn(new FileInputStream("test2.txt"));
+		Solution3.main(null);
+	}
+	@Test
+	public void test3() throws FileNotFoundException {
+		System.setIn(new FileInputStream("test3.txt"));
+		Solution3.main(null);
+	}
+	@Test
+	public void test4() throws FileNotFoundException {
+		System.setIn(new FileInputStream("test4.txt"));
+		Solution3.main(null);
+	}
+
+}
