f965faed5e767154738870827c658e094a9b7100
machenbach <machenbach@vmware.com>
2016-02-18 16:31:23 -0800

    starting

--- cointable/src/Solution.java
+++ cointable/src/Solution.java
@@ -1,12 +1,25 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Solution {
 	int n, m, k;
+	String[] map;
 	
-	public void init (Scanner in) {
-		
+	public void init () throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner in = new Scanner(br.readLine());
+		n = in.nextInt();
+		m = in.nextInt();
+		k = in.nextInt();
+		in.close();
+		map = new String[n];
+		for (int i = 0; i < n; i++) {
+			map[i] = br.readLine();
+		}
 	}
 
 	public static void main(String[] args) {
