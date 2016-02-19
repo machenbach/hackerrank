5d356b1c62d70b94b3bf66815ad6bb6e21bf8dfb
machenbach <machenbach@vmware.com>
2016-02-18 16:22:16 -0800

    First commit

---
+++ cointable/src/Solution.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class Solution {
+	int n, m, k;
+	
+	public void init (Scanner in) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
